89368f02a54c6209c3436088b954fac4c13df46a
Jordinosoft <128376930+Jordinosoft@users.noreply.github.com>
2023-07-12 14:33:48 +0100

    Update login.php

--- login.php
+++ login.php
@@ -14,6 +14,8 @@
     <link rel="stylesheet" href="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="registration.css">
     <link rel="stylesheet" href="all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body style="margin: 0; padding: 0;">
 <header class="navbar navbar-expand-sm">
